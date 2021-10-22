3cde243c5a1cdcf967955bbd91e865b4a7f86558
Jorian <jorian@mail.ru>
2021-10-22 13:33:12 +0300

    actionband 1

--- js/main.js
+++ js/main.js
@@ -2,4 +2,63 @@
     $('.catmenu').click(function(){
         $(this).toggleClass('open');
     })
+    
+    setInterval(retimer, 500);
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function retimer() {
+    let limit = new Date($('.retaimer').data('fordate'));
+    let now = new Date();
+    let delta = Math.floor((limit.getTime() - now.getTime()) / 1000);
+    let sec = delta % 60
+    $('.retaimer .num')[3].innerHTML = `${addChar(sec)}<span class="subnum">${multiple(sec, ['секунда', 'секунды', 'секунд'])}</span>`;
+
+
+
+
+
+
+}
+function addChar(c) {
+    c += '';
+    if (c.length < 2) {
+        c = '0' + c;
+    }
+    return c;
+}
+function multiple(num, words) {
+    num = num % 100;
+    if (Math.floor(num / 10) != 1) {
+        if (num % 10 == 1) {
+            return words[0];
+        } else if ((num % 10 > 1) && (num % 10 < 5)) {
+            return words[1];
+        }
+    }
+    return words[2];
+}
