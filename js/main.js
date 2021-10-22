0cd2397ab2e67592f1cbe1e0fc2536f57a7d0a28
Jorian <jorian@mail.ru>
2021-10-22 14:44:20 +0300

    slider 1

--- js/main.js
+++ js/main.js
@@ -3,46 +3,39 @@
         $(this).toggleClass('open');
     })
     
+    retimer();
     setInterval(retimer, 500);
+    
+    setInterval(slideLeft, 5000);
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function slideLeft() {
+    let hlp = $('.slider_block').index($('.slider_block.next')) + 1;
+    if (hlp > $('.slider_block').length - 1) hlp -= $('.slider_block').length;
+    $('.slider_block.prev').removeClass('prev');
+    $('.slider_block.curr').removeClass('curr').addClass('prev');
+    $('.slider_block.next').removeClass('next').addClass('curr');
+    $('.slider_block').eq(hlp).addClass('next');
+}
+function slideRight() {
+    
+}
 function retimer() {
     let limit = new Date($('.retaimer').data('fordate'));
     let now = new Date();
     let delta = Math.floor((limit.getTime() - now.getTime()) / 1000);
-    let sec = delta % 60
+    if (delta < 0) delta = 0;
+    let sec = delta % 60;
     $('.retaimer .num')[3].innerHTML = `${addChar(sec)}<span class="subnum">${multiple(sec, ['секунда', 'секунды', 'секунд'])}</span>`;
-
-
-
-
-
-
+    delta = Math.floor(delta / 60);
+    let minute = delta % 60;
+    $('.retaimer .num')[2].innerHTML = `${addChar(minute)}<span class="subnum">${multiple(minute, ['минута', 'минуты', 'минут'])}</span>`;
+    delta = Math.floor(delta / 60);
+    let hour = delta % 24;
+    $('.retaimer .num')[1].innerHTML = `${addChar(hour)}<span class="subnum">${multiple(hour, ['час', 'часа', 'часов'])}</span>`;
+    delta = Math.floor(delta / 24);
+    $('.retaimer .num')[0].innerHTML = `${delta}<span class="subnum">${multiple(delta, ['день', 'дня', 'дней'])}</span>`;
 }
 function addChar(c) {
     c += '';
